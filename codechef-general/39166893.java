614a31ef2891c3e0fe9b1a13be0e0d1487fde22b
sreekanth025 <vs25@iitbbs.ac.in>
2020-10-28 14:00:43 +0530

    first commit

---
+++ codechef-general/39166893.java
@@ -0,0 +1,40 @@
+/* Problem Link: https://www.codechef.com/APO12020/problems/APOC2_03 */
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in); 
+		
+		int n = sc.nextInt(); 
+		int k = sc.nextInt(); 
+		
+		int[] arr = new int[n]; 
+		for(int i=0; i<n; i++) {
+		    arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+			
+// 		System.out.println(n); 
+// 		System.out.println(k);
+// 		for(int i=0; i<n; i++) {
+// 		    System.out.printf("%d ", arr[i]);
+// 		}
+
+        Arrays.sort(arr);
+        int ans = Integer.MAX_VALUE;
+        
+        for(int i=0; i<n-k; i++) {
+            ans = Math.min(ans, arr[i+k-1]-arr[i]);
+        }
+	    
+	    System.out.println(ans);	
+	}
+}
---
+++ codechef-general/39167023.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static Scanner sc = new Scanner(System.in); 
+    
+    static String solve() {
+        
+        int n = sc.nextInt();
+        String str = sc.next();
+        
+        
+        for(int i=0; i<n-1; i++) {
+            if(str.charAt(i) == str.charAt(n-1))
+                return "YES";
+        }
+        
+        return "NO";
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int tc = sc.nextInt();
+		
+		while(tc-- > 0) {
+		    System.out.println(solve()); 
+		}
+	}
+}
