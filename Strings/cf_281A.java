46dbea19c00fa15eec0626952d33bfe85ecc9354
Sreekanth <62349142+sreekanth025@users.noreply.github.com>
2020-11-04 20:36:14 +0530

    Create cf_281A.java

---
+++ Strings/cf_281A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public final class cf_281A.java
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in); 
+		char[] str = sc.next().toCharArray(); 
+        if('a' <= str[0] && str[0] <= 'z') {
+            str[0] = (char) (str[0]-'a'+'A');
+        }
+        System.out.println(new String(str)); 
+	}
+}
